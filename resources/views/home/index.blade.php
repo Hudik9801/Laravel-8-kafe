7291cc129c04df5079bde72e1e9f093fb97522a0
Hudik9801 <2017710206021@ogrenci.karabuk.edu.tr>
2021-11-27 16:33:43 +0300

    first commit

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,1 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel8kafej</title>
+</head>
+<body>
+
+<h1>Welcome to laravel </h1>
+<p>This is a paragraph.</p>
+<br>
+<a href="{{route('test',['id'=>12,'name'=>'Fatma'])}}">Test Sayfa</a>
+
+
+</body>
+</html>
